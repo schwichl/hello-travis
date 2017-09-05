58580ef57b2f7b732eea7a55b4fe772627f8bda4
lukas <schwichl@students.uni-marburg.de>
2017-09-05 15:23:34 +0200

    aufgabe 1

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,4 +1,5 @@
 
+
 /**
  * A class containing a main method which prints "Hello Travis!"
  * @author Johannes Drönner
@@ -18,9 +19,21 @@ public class Hello {
 	 * Main method
 	 * @param args not used
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		System.out.println(sayHelloTravis()+"!");
+		System.out.println(DateTools.dayOfTheWeek(5, 9, 2017));
+		
+		if (false)
+		{
+  		for (int i = 1; i <= 12; ++i)
+  		{
+  		  System.out.println(i + " has " + DateTools.daysPerMonth(i, 2017) + " days");
+  		}
+		}
 
+    System.out.println(DateTools.dayOfTheYear(2, 2, 2017) + " is der tag");
+    System.out.println(DateTools.daysTillXmas(25, 12, 2017) + " tage bis weihnachten");
 	}
 
 }
