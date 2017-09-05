4b6bee2b6d92e11507fefbf0c5a63b36a44de176
lukas <schwichl@students.uni-marburg.de>
2017-09-05 16:08:13 +0200

    aufgabe 2.3

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -15,25 +15,53 @@ public class Hello {
 		return "Hello Travis xd";
 	}; 
 
+	private static void Aufgabe1()
+	{
+	  System.out.println(sayHelloTravis()+"!");
+	  System.out.println(DateTools.dayOfTheWeek(5, 9, 2017));
+	  
+	  for (int i = 1; i <= 12; ++i)
+	  {
+	    System.out.println(i + " has " + DateTools.daysPerMonth(i, 2017) + " days");
+	  }
+	  
+
+	  System.out.println(DateTools.dayOfTheYear(2, 2, 2017) + " is der tag");
+	  System.out.println(DateTools.daysTillXmas(25, 12, 2017) + " tage bis weihnachten");
+	}
+	
+	private static void Aufgabe2()
+	{
+	  // CastingTools.printChars();
+	  System.out.println(CastingTools.stringToInt("444"));
+	}
+	
+	private static boolean containsPattern(char[] chars, char[] pattern)
+	{
+	  return chars.toString().contains(pattern.toString());
+	}
+	
+	private static long convertBinaryToDecimal(String binaryNumber)
+	{
+	  int decimal = 0;
+	  
+	  for (int i = 0; i < binaryNumber.length(); ++i)
+	  {
+	    decimal += Integer.valueOf(binaryNumber.charAt(i) - '0') * Math.pow(2, ((binaryNumber.length() - i) - 1));
+	  }
+	  
+	  return decimal;
+	}
+	
 	/**
 	 * Main method
 	 * @param args not used
 	 */
 	public static void main(String[] args)
 	{
-		System.out.println(sayHelloTravis()+"!");
-		System.out.println(DateTools.dayOfTheWeek(5, 9, 2017));
-		
-		if (false)
-		{
-  		for (int i = 1; i <= 12; ++i)
-  		{
-  		  System.out.println(i + " has " + DateTools.daysPerMonth(i, 2017) + " days");
-  		}
-		}
-
-    System.out.println(DateTools.dayOfTheYear(2, 2, 2017) + " is der tag");
-    System.out.println(DateTools.daysTillXmas(25, 12, 2017) + " tage bis weihnachten");
+	  // Aufgabe2();
+	  
+	  System.out.println(convertBinaryToDecimal("1100"));
 	}
 
 }
